b51d00fb6f9d2a44496b9aa570773dc514a0df1f
Andy Lee <a.lee.intl@gmail.com>
2023-12-22 18:12:33 +0100

    configure vite using node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { resolve } from "path"
+import { execSync } from "child_process"
+
+// const __DEV__ = process.env.NODE_ENV === "development"
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim()
+const commitDateCommand =
+  'git log -1 --format=%cd --date=format:"%H:%M %d-%B-%Y"'
+const commitDateTime = execSync(commitDateCommand).toString().trim()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+    __COMMIT_DATETIME__: JSON.stringify(commitDateTime),
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
