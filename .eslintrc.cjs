a24de96deb0a08c66fc7ff9981ed85193325a220
Andy Lee <a.lee.intl@gmail.com>
2023-12-22 17:12:25 +0100

    configure eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:eslint-comments/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    // Typescript rules
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": "off",
+
+    // Eslint rules
+    "no-empty": "off",
+    "prefer-const": "warn",
+    "no-extra-semi": "off",
+    "no-extra-boolean-cast": "off",
+
+    // Eslint-comments rules
+    "eslint-comments/no-unused-disable": "warn",
+
+    // React rules
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
